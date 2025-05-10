e54a79060ee4f34e1f43ab934cb32166f7e4063d
Tanuj <tanujtkmp@gmail.com>
2025-05-10 12:30:11 +0800

    Initital Commit

---
+++ bondConfiguration.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+
+// Read the periodic table
+const ptable = JSON.parse(fs.readFileSync('PeriodicTableJSON.json'))
+// const input = prompt("Molecule: ")
+const formula = "H2SO4"
+
+// Parsing the formula => "H2O" -> [{symbol: 'H', count: 2}, ...]
+const elements = [];
+const regex = /([A-Z][a-z]*)(\d*)/g;
+let match;
+id = 0;
+
+while ((match = regex.exec(formula)) !== null) {
+    const symbol = match[1];
+    const count = match[2] ? parseInt(match[2]) : 1;
+    if(count != 0){
+        for(let i = 0; i < count; i++){
+            elements.push({symbol, id})
+            id ++;
+        }
+    }
+    else {{elements.push({ symbol, count });}}
+}
+
+// Generate valency count for each atom and electronegativity value
+
+// Add the index of the element into the elements list to reduce time for future lookups
+const lastDigit = (num) => num % 10;
+for(let element = 0; element < elements.length; element++){
+    let elementSymbol = elements[element]['symbol'];
+    for(let i = 0; i < 118; i++){
+        if(ptable["elements"][i]["symbol"] == elementSymbol){
+            elements[element]["index"] = i;
+        }
+    }
+
+    elements[element]["valence"] = 8 - (lastDigit(ptable["elements"][elements[element]["index"]]["group"]))
+    if(ptable["elements"][elements[element]["index"]]["electronegativity_pauling"] == null) {
+        console.log("Electronegatvity invalid")
+    } else {
+        elements[element]["electronegativity"] = ptable["elements"][elements[element]["index"]]["electronegativity_pauling"]
+    }
+}
+
+// Identiy Central Atom based on electronegativity
+let leastEN = 3.98;
+let centralAtom = "";
+for(let element = 0; element < elements.length; element++){
+    if(elements[element]["electronegativity"] < leastEN && elements[element]["symbol"] != "H"){
+        leastEN = elements[element]["electronegativity"]
+        centralAtom = elements[element]
+    }
+    if(centralAtom == elements[element]["symbol"]){
+        elements[element]["type"] = "central"
+    }
+    else {elements[element]["type"] = "non"}
+} 

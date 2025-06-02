38fb78aee3998038ca1770679787aae3b39d13fc
Tanuj <tanujtkmp@gmail.com>
2025-06-02 22:43:23 +0800

    Add a function to check whether or not two atoms are bondable based on valency

--- bondConfiguration.js
+++ bondConfiguration.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 // Read the periodic table
 const ptable = JSON.parse(fs.readFileSync('PeriodicTableJSON.json'))
 // const input = prompt("Molecule: ")
-const formula = "H2SO4"
+const formula = "H2O"
 
 // Parsing the formula => "H2O" -> [{symbol: 'H', count: 2}, ...]
 const elements = [];
 const regex = /([A-Z][a-z]*)(\d*)/g;
 let match;
 let id = 0;
-let centralIndex = 0;
+const bonds = [
+
+];
 
 while ((match = regex.exec(formula)) !== null) {
     const symbol = match[1];
@@ -46,12 +48,30 @@ for(let element = 0; element < elements.length; element++){
 
 // Identiy Central Atom based on electronegativity
 let maxEN = 3.98;
-let centralAtom = {};
+let centralAtom = 0;
 for(let element = 0; element < elements.length; element++){
     let x = elements[element];
     if(x["electronegativity"] < maxEN && x['symbol'] != 'H'){
         maxEN = x['electronegativity']
         x['type'] = 'cen'
+        centralAtom = element
     }
     else {x['type'] = 'non'}
+}
+
+function canBond(atom1, atom2, currentBonds1, currentBonds2){
+    let v1 = atom1['valence'];
+    let v2 = atom2['valence'];
+    
+    if(atom1['symbol'] == 'H'){
+        let maxBond1 = 1;
+    }
+    else if(atom2['symbol'] == 'H'){
+        let maxBond2 = 1;
+    }
+    else{
+        let maxBond1 = 8 - v1;
+        let maxBond2 = 8 - v2;
+    }
+    return (currentBonds1 < maxbond1) && (currentBonds2 < maxBonds2)
 }
